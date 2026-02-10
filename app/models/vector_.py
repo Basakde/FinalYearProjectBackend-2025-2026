c6d1f34d80a96676fda1502a33f0265dd9c05711
Basak Deveci <D22124576@mytudublin.ie>
2026-02-10 20:51:26 +0000

    Vector structured is created for item vector encodings.

---
+++ app/models/vector_.py
@@ -0,0 +1,20 @@
+# app/services/vector_manual.py
+
+COLORS = [
+    "black","white","grey","blue","red","green","beige","brown","pink","purple",
+    "yellow","orange","navy","turquoise","burgundy","mustard","other" #17
+]
+
+MATERIALS = [
+    "cotton","denim","wool","leather","polyester","linen","silk","elastane",
+    "nylon","cashmere","viscose","knit","suede","alpaca","other" #15
+]
+
+OCCASIONS = [
+    "casual","work","formal","sport","party","travel","outdoors","home","date",
+    "school","dinner","other" #12
+]
+
+SEASONS = ["spring","summer","autumn","winter"]  # 4 fixed
+
+CATEGORIES = ["top","bottom","outerwear","shoes","accessory","jumpsuit"] #6
